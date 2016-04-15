240a380ecaec9372eeef57fe70882e0649c3d00f
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2016-04-15 10:43:05 +0200

    adding new file

---
+++ R/get_values.R
@@ -0,0 +1,15 @@
+
+
+# http://widukind-api.cepremap.org/api/v1/json/datasets/insee-cho-an-halo/values?sexe=1&age=15
+
+test <- "datasets/insee-cho-an-halo/values?sexe=1&age=15" %>% 
+  paste0(api, .) %>% 
+  fromJSON() %>% 
+  select_data()
+
+
+test <- "datasets/insee-cho-an-halo/values?sexe=1&age=15" %>% 
+  paste0(api, .) %>% 
+  fromJSON() %>% 
+  select_data() %>% 
+  select_values()
